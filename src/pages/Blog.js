b453a2042bb3e371dff3af88d470bf99b8fa2f05
Rajender Utthari <rajender.u@gmail.com>
2022-01-09 02:01:30 +0530

    First commit

---
+++ src/pages/Blog.js
@@ -0,0 +1,5 @@
+const Blogs = () => {
+  return <h1>Blog</h1>;
+};
+
+export default Blogs;
---
+++ src/pages/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      Copyrights @2022 
+    </div>
+  );
+}
+
+export default Footer;
